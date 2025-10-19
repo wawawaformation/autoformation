4e06af9b9deab3e69c4021f223e0a2363d9afbbd
david <wawawaformation@gmail.com>
2025-10-19 18:49:57 +0200

    a classse sans erreur;

--- tests/MaClasseTest.php
+++ tests/MaClasseTest.php
@@ -1,14 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests;
+
 use App\MaClasse;
 use PHPUnit\Framework\TestCase;
+
 class MaClasseTest extends TestCase
 {
     public function testDisBonjour()
     {
         $maClasse = new MaClasse();
         $resultat = $maClasse->disBonjour('david');
-        $this->assertEquals('Hello, david!', $resultat);
+        $this->assertEquals('Hello, David!', $resultat);
     }
 }
