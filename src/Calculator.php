ba0eeae82c6aa8f535a1d95431a98a6ca7db09a8
david <wawawaformation@gmail.com>
2025-10-19 16:19:17 +0200

    cs-fixer corrigé

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App;
 
-class Calculator{
-    public function add(float $a, float $b): float{
+class Calculator
+{
+    public function add(float $a, float $b): float
+    {
         return $a + $b;
     }
 
-    public function divide(float $a, float $b): float{
+    public function divide(float $a, float $b): float
+    {
         if ($b == 0) {
             throw new \InvalidArgumentException('Division by zero');
         }
